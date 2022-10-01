3235b33287715f0c66acccea9427e15d0f1a8ca4
overgrownFudge <overgrownfudge@gmail.com>
2022-10-01 19:23:57 -0400

    added cross origin config

--- backend/src/main/java/com/portfolioproject/backend/experience/ExperienceController.java
+++ backend/src/main/java/com/portfolioproject/backend/experience/ExperienceController.java
@@ -1,6 +1,7 @@
 package com.portfolioproject.backend.experience;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping(path = "api/v1/experience")
 public class ExperienceController {
 
